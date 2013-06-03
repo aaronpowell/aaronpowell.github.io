af70ba8be252c1982a158da339cd3a5d1c553687
Aaron Powell <me@aaron-powell.com>
2013-06-03 22:24:08 +1000

    adding express.js app launcher

---
+++ plugins/docpad-plugin-staticroutes/staticroutes.plugin.js
@@ -0,0 +1,53 @@
+var __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) {
+        for (var key in parent) {
+            if (__hasProp.call(parent, key)) 
+                child[key] = parent[key];
+        }
+        function ctor() {
+            this.constructor = child;
+        }
+        ctor.prototype = parent.prototype;
+        child.prototype = new ctor();
+        child.__super__ = parent.prototype; return child;
+    };
+
+module.exports = function (BasePlugin) {
+    var _ref, StaticRoutes,
+        fs = require('fs'),
+        path = require('path');
+
+    return StaticRoutes = (function (_super) {
+        __extends(StaticRoutes, _super);
+
+        function StaticRoutes() {
+          _ref = StaticRoutes.__super__.constructor.apply(this, arguments);
+          return _ref;
+        }
+
+        StaticRoutes.prototype.name = 'staticroutes';
+
+        StaticRoutes.prototype.renderBefore = function(opts, next) {
+            var collection = opts.collection;
+
+            var docs = collection.toJSON();
+
+            var routes = docs.map(function (doc) {
+                return {
+                    url: doc.url,
+                    redirects: doc.urls
+                };
+            });
+
+            fs.writeFile(
+                path.join(this.docpad.config.outPath, 'routes.json'),
+                JSON.stringify({
+                    routes: routes
+                }),
+                next
+            );
+        };
+
+        return StaticRoutes;
+    })(BasePlugin);
+};
---
+++ web.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/out'));
+
+var routes = require('./out/routes.json').routes;
+
+var redirector = function (dest) {
+    return function (req, res) {
+        res.redirect(301, dest);
+    };
+};
+
+routes.map(function (route) {
+    if (route.redirects) {
+        return route.redirects.map(function (redirect) {
+            return app.get(redirect, redirector(route.url));
+        });
+    }
+    return;
+});
+
+app.listen(process.env.PORT || 3000);
