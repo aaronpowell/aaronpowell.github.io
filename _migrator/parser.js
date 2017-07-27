70739727d73e78f06854f4acca14744af7c0c776
Aaron Powell <me@aaron-powell.com>
2017-07-27 17:58:52 +1000

    new parser for hugo

---
+++ _migrator/parser.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const cson = require('cson');
+const json2yaml = require('json2yaml');
+
+const postsPath = path.join(__dirname, '..', 'src', 'documents', 'posts');
+
+fs.readdir(postsPath, (err, paths) => {
+    paths.forEach((filename) => {
+        let post = fs.readFileSync(path.join(postsPath, filename), 'utf8').split('\r\n');
+        
+        let header = [];
+        let body = [];
+        
+        let inHeader = false;
+        for (let i = 0; i < post.length; i++) {
+            let line = post[i];
+        
+            if (line === '--- cson') {
+                inHeader = true;
+                continue;
+            } else if (line === '---') {
+                inHeader = false;
+                continue;
+            }
+        
+            if (inHeader) {
+                header.push(line);
+            } else {
+                body.push(line);
+            }
+        }
+        
+        const headerObject = cson.parse(header.join('\r\n'));
+        
+        const newPost =
+            json2yaml.stringify(headerObject) +
+            '---\r\n' +
+            body.join('\r\n');
+
+        fs.writeFileSync(path.join(__dirname, '_posts', filename.replace('.html.', '.')), newPost, 'utf8');
+        
+    });
+});
