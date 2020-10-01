236794cedf3d12113639d27b97ee8edb9d7460fc
Aaron Powell <me@aaron-powell.com>
2020-10-01 14:11:13 +1000

    starting on a copy link

--- src/themes/aaronpowell/assets/js/script.js
+++ src/themes/aaronpowell/assets/js/script.js
@@ -1,11 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var toggle = document.querySelector("header .toggle");
-    toggle.addEventListener("click", function(e) {
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector("header .toggle");
+    toggle.addEventListener("click", function (e) {
         e.preventDefault();
         toggle.parentElement.classList.toggle("active");
 
-        var i = toggle.querySelector("i");
+        const i = toggle.querySelector("i");
         i.classList.toggle("ion-md-menu");
         i.classList.toggle("ion-md-close");
     });
+
+    const copy = document.querySelectorAll(".copy");
+    const handleCopy = async (e) => {
+        const item = e.target;
+        const target = document.querySelector(item.dataset["target"]);
+        await window.navigator.clipboard.writeText(target.innerHTML);
+        const copied = item.parentElement.querySelector(".copied");
+        copied.style.display = "inline";
+        setTimeout(() => {
+            copied.style.display = "none";
+        }, 1000);
+    };
+
+    for (const item of copy) {
+        item.addEventListener("click", handleCopy);
+    }
 });
